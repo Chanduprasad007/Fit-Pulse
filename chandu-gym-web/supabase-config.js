38b0bb20af40e2c291af73cb3306a2e46263f5bd
Chanduprasad007 <31735042+Chanduprasad007@users.noreply.github.com>
2026-04-09 16:52:44 +0530

    Update supabase-config.js

--- chandu-gym-web/supabase-config.js
+++ chandu-gym-web/supabase-config.js
@@ -1,5 +1,5 @@
 export const SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://dzmrmkxottxmgfprohuv.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6bXJta3hvdHR4bWdmcHJvaHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2OTgyODQsImV4cCI6MjA5MTI3NDI4NH0.Vx3qdXeFazUs40uyJC3Konuj5wJ-jqp9Fj4yz31EvIs",
   redirectTo: "https://chanduprasad007.github.io/Fit-Pulse/",
 };
